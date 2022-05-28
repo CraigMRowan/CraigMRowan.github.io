23254e8b46b18eb875174ef57058adc6864f67d8
Craig Rowan <craigmrowan@gmail.com>
2022-05-28 16:06:43 +1200

    Changed site to use w3.css

--- web/scripts/scripts.js
+++ web/scripts/scripts.js
@@ -1,12 +1,28 @@
-/*Google Map*/
+// Tabbed Menu
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  }
+  document.getElementById("myLink").click();
+
+// Google Map 
 function initMap() {
-    var op = {lat: -35.227488, lng: 173.948835};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 9,
-      center: op
-    });
-    var marker = new google.maps.Marker({
-      position: op,
-      map: map
-    });
+  var op = {lat: -35.227488, lng: 173.948835};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: op
+  });
+  var marker = new google.maps.Marker({
+    position: op,
+    map: map
+  });
 }
